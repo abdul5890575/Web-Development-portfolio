a7d93e3df8e2c1154f3589638b9c457aa49adcaf
Abdul rehman Shaukat <59484730+abdul5890575@users.noreply.github.com>
2020-07-21 14:15:15 -0400

    Add files via upload
    First Website

---
+++ js/gallery.js
@@ -0,0 +1,17 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+
+   
+                                                           
+    document.getElementById('image').style.backgroundImage="url(" +previewPic.src+") ";
+    document.getElementById('image').innerHTML = previewPic.alt;
+  
+	}
+
+	function unDo(){
+        document.getElementById('image').style.backgroundImage="url('')";
+        document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+
+		
+	}
